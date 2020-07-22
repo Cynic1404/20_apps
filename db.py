be25bc09d235d0fbcad8498e866c535389143808
Mikhail Nekhaenok <mnekhaenok@way2b1.com>
2020-07-22 14:18:20 -0700

    code refactoring

--- db.py
+++ db.py
@@ -26,49 +26,44 @@ def add_row(task='This is string field!', deadline=datetime.today().date()):
 
 
 
-def show_tasks():
-    rows = connect_db().query(Table).order_by(Table.deadline).all()
-    if len(rows) == 0:
-        print('Nothing to do!')
+def show_tasks(date=None):
+    if date:
+        rows = connect_db().query(Table).filter(Table.deadline == datetime.strptime(date, '%Y-%m-%d').date()).all()
     else:
-        for i in range(len(rows)):
-            print(f'{str(i + 1)}. {str(rows[i])} {rows[i].deadline.strftime("%d %b")}')
+        rows = connect_db().query(Table).order_by(Table.deadline).all()
+    print_rows(rows)
     return rows
-
-def show_filtered_tasks(date):
-    rows = connect_db().query(Table).filter(Table.deadline == datetime.strptime(date, '%Y-%m-%d').date()).all()
-    if len(rows) == 0:
-        print('Nothing to do!')
-    else:
-        for i in range(len(rows)):
-            print(str(i + 1)+'. '+ str(rows[i]))
-
 
 def show_missed_tasks():
     rows = connect_db().query(Table).filter(Table.deadline < datetime.date(datetime.today())).order_by(Table.deadline).all()
-    if len(rows) == 0:
-        print('Nothing to do!')
-    else:
-        for i in range(len(rows)):
-            print(f'{str(i + 1)}. {str(rows[i])} {rows[i].deadline.strftime("%d %b")}')
-        print('\n')
+    print_rows(rows)
 
 
 def weeks_tasks():
     for i in range(7):
         print((datetime.today() + timedelta(days=i)).strftime('%A %d %b'))
-        show_filtered_tasks((datetime.today()+timedelta(days=i)).strftime('%Y-%m-%d'))
-        print('\n')
+        show_tasks(date=(datetime.today()+timedelta(days=i)).strftime('%Y-%m-%d'))
+
 
 def delete_task():
     delete_session = connect_db()
-    print('Chose the number of the task you want to delete:')
     rows = delete_session.query(Table).order_by(Table.deadline).all()
-    row_to_delete = rows[int(input(''))-1]
-    delete_session.delete(row_to_delete)
-    delete_session.commit()
+    if len(rows) == 0:
+        print('Nothing to delete \n')
+    else:
+        print('Chose the number of the task you want to delete:')
+        print_rows(rows)
+        row_to_delete = rows[int(input(''))-1]
+        delete_session.delete(row_to_delete)
+        delete_session.commit()
 
-
+def print_rows(rows):
+    if len(rows) == 0:
+        print('Nothing to do!')
+    else:
+        for i in range(len(rows)):
+            print(f'{str(i + 1)}. {str(rows[i])} {rows[i].deadline.strftime("%d %b")}')
+    print('\n')
 
 def connect_db():
     engine = create_engine('sqlite:///todo.db?check_same_thread=False')
@@ -89,7 +84,7 @@ if __name__ == '__main__':
         option = input('\n')
         if option == '1':
             print(f'Today {datetime.today().strftime("%d %b")}')
-            show_filtered_tasks(datetime.now().strftime('%Y-%m-%d'))
+            show_tasks(datetime.now().strftime('%Y-%m-%d'))
         elif option == '2':
             weeks_tasks()
         elif option == '3':
@@ -97,14 +92,11 @@ if __name__ == '__main__':
         elif option == '4':
             show_missed_tasks()
         elif option == '5':
-            add_row(task=input('Enter task\n'), deadline=datetime.strptime(input('Enter deadline\n'),'%Y-%m-%d').date())
+            add_row(task=input('Enter task\n'), deadline=datetime.strptime(input('Enter deadline. Format YYYY-MM-DD\n'),'%Y-%m-%d').date())
             print('The task has been added!')
         elif option == '6':
             delete_task()
         elif option == '0':
-            engine = create_engine('sqlite:///todo.db?check_same_thread=False')
-            Base.metadata.create_all(engine)
-            session = sessionmaker(bind=engine)
             quit()
         else:
             print('wrong option, try again\n')
