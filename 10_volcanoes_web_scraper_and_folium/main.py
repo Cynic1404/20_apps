72d29d74450623c8fb109b94c229d9d99bf35364
mikhail.nekhaenok <mikhail.nekhaenok@bytedance.com>
2023-10-18 15:16:27 -0700

    add streamlit support

--- 10_volcanoes_web_scraper_and_folium/main.py
+++ 10_volcanoes_web_scraper_and_folium/main.py
@@ -1,7 +1,8 @@
 import requests
 import selectorlib
 import folium
-
+import streamlit as st
+from streamlit_folium import st_folium
 urls = ["https://volcano.oregonstate.edu/volcano_table", "https://volcano.oregonstate.edu/volcano_table?page=1",
         "https://volcano.oregonstate.edu/volcano_table?page=2"]
 
@@ -36,16 +37,19 @@ def color_detector(height):
 
 
 if __name__ == "__main__":
+    st.title("Volcano Table")
+
+
     all_volcanoes = []
     for page in urls:
         volcanoes_from_page = extract(scrape(page))
         all_volcanoes += volcanoes_from_page
 
     map = folium.Map(zoom_start=10)
-    fg = folium.FeatureGroup(name="Volcanoes")
+
     for volacno in all_volcanoes:
         if all([i != "" for i in volacno]):
-            fg.add_child(folium.Marker(location=[volacno[3], volacno[4]], popup=volacno[0],
-                                       icon=folium.Icon(color=color_detector(volacno[5]))))
-            map.add_child(fg)
-    map.save("VOLCANOES.html")
+            folium.Marker(location=[volacno[3], volacno[4]], popup=volacno[0],
+                                       icon=folium.Icon(color=color_detector(volacno[5]))).add_to(map)
+    st_data = st_folium(map, width=700)
+    # map.save("VOLCANOES.html")
